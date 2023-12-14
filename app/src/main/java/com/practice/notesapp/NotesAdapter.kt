5f9aaf6e9f2f905c97c7e71915a0fc9d9dc06f47
YashNagare <yashpnagare@gmail.com>
2023-12-14 13:21:38 +0530

    Note Update Functionality

--- app/src/main/java/com/practice/notesapp/NotesAdapter.kt
+++ app/src/main/java/com/practice/notesapp/NotesAdapter.kt
@@ -1,9 +1,11 @@
 package com.practice.notesapp
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,6 +15,7 @@ class NotesAdapter(private var notes: List<Note>, context: Context) :
     class NoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+        val updateButton: ImageView = itemView.findViewById(R.id.updateButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
@@ -28,6 +31,14 @@ class NotesAdapter(private var notes: List<Note>, context: Context) :
         val note = notes[position]
         holder.titleTextView.text = note.title
         holder.contentTextView.text = note.content
+
+        holder.updateButton.setOnClickListener {
+            val intent = Intent(holder.itemView.context, UpdateNoteActivity::class.java).apply {
+                putExtra("note_id", note.id)
+            }
+            holder.itemView.context.startActivity(intent)
+        }
+
     }
 
     fun refreshData(newNotes: List<Note>) {
