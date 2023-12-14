17dc4be9764b4dff7e1dbc5933fe2a5683cdf173
YashNagare <yashpnagare@gmail.com>
2023-12-14 13:38:21 +0530

    Note Delete Functionality

--- app/src/main/java/com/practice/notesapp/NotesDatabaseHelper.kt
+++ app/src/main/java/com/practice/notesapp/NotesDatabaseHelper.kt
@@ -85,4 +85,12 @@ class NotesDatabaseHelper(context: Context) :
         return Note(id, title, content)
     }
 
+    fun deleteNote(noteId: Int){
+        val db = writableDatabase
+        val whereClause = "$COLUMN_ID = ?"
+        val whereArgs = arrayOf(noteId.toString())
+        db.delete(TABLE_NAME, whereClause, whereArgs)
+        db.close()
+    }
+
 }
